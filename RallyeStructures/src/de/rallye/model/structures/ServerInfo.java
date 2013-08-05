3cad429d97ce52ae94d58bc0bf26990095a72247
Ramon Wirsch <ramon.wirsch@stud.tu-darmstadt.de>
2013-08-05 02:54:19 +0200

    + new Class ServerInfo.Api

--- RallyeStructures/src/de/rallye/model/structures/ServerInfo.java
+++ RallyeStructures/src/de/rallye/model/structures/ServerInfo.java
@@ -4,18 +4,28 @@ public class ServerInfo {
 
 	public static final String NAME = "name";
 	public static final String DESCRIPTION = "description";
-	public static final String API_NAME = "apiName";
-	public static final String API_VERSION = "apiVersion";
+	public static final String API = "api";
 	
 	final public String name;
 	final public String description;
-	final public String[] apiName;
-	final public int[] apiVersion;
+	final public Api[] api;
 	
-	public ServerInfo(String name, String description, String[] apiName, int[] apiVersion) {
+	public static class Api {
+		public static final String NAME = "name";
+		public static final String VERSION = "version";
+		
+		final public String name;
+		final public int version;
+		
+		public Api(String name, int version) {
+			this.name = name;
+			this.version = version;
+		}
+	}
+	
+	public ServerInfo(String name, String description, Api[] api) {
 		this.name = name;
 		this.description = description;
-		this.apiName = apiName;
-		this.apiVersion = apiVersion;
+		this.api = api;
 	}
 }

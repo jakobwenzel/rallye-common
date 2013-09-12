7ad0025849efc3df8e1168c54af6d510dfbdf662
Ramon Wirsch <ramon.wirsch@stud.tu-darmstadt.de>
2013-09-12 20:23:26 +0200

    * MapConfig now complete

--- RallyeStructures/src/main/java/de/rallye/model/structures/MapConfig.java
+++ RallyeStructures/src/main/java/de/rallye/model/structures/MapConfig.java
@@ -1,24 +1,36 @@
 package de.rallye.model.structures;
 
+import java.util.List;
+
 public class MapConfig {
 
 	public static final String NAME = "name";
 	public static final String LOCATION = "location";
 	public static final String ZOOM_LEVEL = "zoomLevel";
+	public static final String BOUNDS = "bounds";
 
 	final public String name;
 	final public LatLng location;
 	final public float zoomLevel;
+	final public List<LatLng> bounds;
 	
-	public MapConfig(String name, double lat, double lon, float zoomLevel) {
+	public MapConfig(String name, LatLng location, float zoomLevel, List<LatLng> bounds) {
 		this.name = name;
-		this.location = new LatLng(lat, lon);
+		this.location = location;
 		this.zoomLevel = zoomLevel;
+		this.bounds = bounds;
+	}
+
+	public MapConfig() {
+		name = null;
+		location = null;
+		zoomLevel = 0;
+		bounds = null;
 	}
 	
 	@Override
 	public String toString() {
-		return "Location : "+ name +"\nCoordinates: "+ location.toString() +"\nZoom: "+ zoomLevel;
+		return "Location : "+ name +"\nLocation: "+ location.toString() +"\nZoom: "+ zoomLevel +"\nBounds: "+ bounds.toString();
 	}
 
 	@Override
@@ -31,6 +43,7 @@ public class MapConfig {
 		if (Float.compare(that.zoomLevel, zoomLevel) != 0) return false;
 		if (!location.equals(that.location)) return false;
 		if (!name.equals(that.name)) return false;
+		if (!bounds.equals(that.bounds)) return false;
 
 		return true;
 	}

ef60ffceebfb7b599da9e3684e802baeaf68526f
Jakob Wenzel <jakobwenzel92@gmail.com>
2013-10-02 22:28:18 +0200

    Added new field in TaskSubmissions to mark a rating as outdated when a new submission is added after rating.

--- RallyeStructures/src/main/java/de/rallye/model/structures/TaskSubmissions.java
+++ RallyeStructures/src/main/java/de/rallye/model/structures/TaskSubmissions.java
@@ -8,20 +8,23 @@ public class TaskSubmissions {
 	public static final String SUBMISSIONS = "submissions";
 	public static final String SCORE = "score";
 	public static final String BONUS = "bonus";
+	public static final String SCORE_OUTDATED = "scoreOutdated";
 	
 	final public int taskID;
 	final public List<Submission> submissions;
 	final public Integer score;
 	final public Integer bonus;
+	final public boolean scoreOutdated;
 	
 	public boolean isRated() {
 		return score != null || bonus != null;
 	}
 
-	public TaskSubmissions(int taskID, List<Submission> submissions, Integer score, Integer bonus) {
+	public TaskSubmissions(int taskID, List<Submission> submissions, Integer score, Integer bonus, boolean scoreOutdated) {
 		this.taskID = taskID;
 		this.submissions = submissions;
 		this.score = score;
 		this.bonus = bonus;
+		this.scoreOutdated = scoreOutdated;
 	}
 }

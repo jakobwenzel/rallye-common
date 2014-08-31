1b22d69cb28eee04773fa563e192dfdf50242017
Ramon Wirsch <ramon.wirsch@stud.tu-darmstadt.de>
2014-08-31 19:47:46 +0200

    + GPL

--- RallyeStructures/src/main/java/de/rallye/model/mapper/EdgeNodeSerializer.java
+++ RallyeStructures/src/main/java/de/rallye/model/mapper/EdgeNodeSerializer.java
@@ -1,10 +1,29 @@
-package de.rallye.model.mapper;
+/*
+ * Copyright (c) 2014 Jakob Wenzel, Ramon Wirsch.
+ *
+ * This file is part of RallySoft.
+ *
+ * RallyeSoft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-import java.io.IOException;
+package de.rallye.model.mapper;
 
 import org.codehaus.jackson.JsonGenerator;
 import org.codehaus.jackson.map.JsonSerializer;
 import org.codehaus.jackson.map.SerializerProvider;
+
+import java.io.IOException;
 
 import de.rallye.model.structures.Node;
 

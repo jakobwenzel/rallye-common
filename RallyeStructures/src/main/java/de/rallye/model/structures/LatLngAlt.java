45b62e788278c31a366b8be2fc1e419c72447d28
Ramon Wirsch <ramon.wirsch@stud.tu-darmstadt.de>
2014-09-18 01:48:16 +0200

    + LatLngAlt
    Note: manual deserialization ist no longer neccessary on newly created classes

---
+++ RallyeStructures/src/main/java/de/rallye/model/structures/LatLngAlt.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2014 Jakob Wenzel, Ramon Wirsch.
+ *
+ * This file is part of RallySoft.
+ *
+ * RallyeSoft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.rallye.model.structures;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by Ramon on 18.09.2014.
+ */
+public class LatLngAlt extends LatLng {
+
+	public final int altitude;
+
+	public LatLngAlt(@JsonProperty("latitude") double lat, @JsonProperty("longitude") double lon, @JsonProperty("altitude") int altitude) {
+		super(lat, lon);
+		this.altitude = altitude;
+	}
+}
